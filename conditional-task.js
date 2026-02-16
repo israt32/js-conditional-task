c9ff0ea8587d3c7b0e7d20488f9611489495e499
Israt Jahan <israt5227@gmail.com>
2026-02-17 01:54:51 +0600

    task 5

--- conditional-task.js
+++ conditional-task.js
@@ -132,3 +132,31 @@ if(myMarks > 80){
 else{
   console.log('sleep and act sad')
 }
+
+
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+var num1 = 18;
+var num2 = 12;
+
+var result;
+
+if(num1 > num2){
+  result = num1*2
+}
+else{
+  result = num1 + num2
+}
+
+result = num1 > num2 ? num1 *2 : num1 + num2;
