82766fd3f097349a0466fb09ad58b462fbed7ce7
Israt Jahan <israt5227@gmail.com>
2026-02-16 22:19:01 +0600

    free drink

---
+++ conditional-task.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+var burger = 200;
+
+if(burger > 500){
+  console.log('you get a free coke')
+}
+else{
+  console.log(burger + 30)
+}
